3da9e9d9a8417f1c2a37c6cb5aa8989e42812a6f
gpfla6022 <gpfla3503@gmail.com>
2021-10-26 20:54:36 +0900

    세팅

---
+++ src/test/java/com/velpe/yoon/sfarm/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.velpe.yoon.sfarm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
