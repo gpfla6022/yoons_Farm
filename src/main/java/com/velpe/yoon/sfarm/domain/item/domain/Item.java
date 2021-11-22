554be1e92b17a0d7f4b2e548ffd92c58a701fb35
gpfla6022 <gpfla3503@gmail.com>
2021-11-22 23:28:19 +0900

    ADD : Item Entity 필드 추가

--- src/main/java/com/velpe/yoon/sfarm/domain/item/domain/Item.java
+++ src/main/java/com/velpe/yoon/sfarm/domain/item/domain/Item.java
@@ -1,15 +1,26 @@
 package com.velpe.yoon.sfarm.domain.item.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Entity
 public class Item {
 
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "item_name")
     private String name;
+
+    private int price;
+
+    private int stock;
+
+    private String desc;
+
+    private LocalDateTime regDate;
+
+    private LocalDateTime updateDate;
+
 }
